0872e2b6f8b7da178f9a5b52485981c77c83d2b3
Erin <erin2722@gmail.com>
2023-04-04 16:08:13 -0400

    Bump version: 0.1.1 → 0.2.0

--- usau_scraper/__init__.py
+++ usau_scraper/__init__.py
@@ -1,4 +1,4 @@
 from .teamScraper import getTeamInfo, getTeamSchedule, getTeamRoster, queryTeam, setArgs, fillInBasicInfo
 
 __all__ = ['getTeamInfo', 'getTeamSchedule', 'getTeamRoster', 'queryTeam', 'setArgs', 'fillInBasicInfo', '__version__']
-__version__ = "0.1.1"
+__version__ = "0.2.0"
